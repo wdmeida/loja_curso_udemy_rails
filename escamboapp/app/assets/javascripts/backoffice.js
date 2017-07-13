a42e757cec5a851d9447f4910e03c7897fba55c8
Wagner Almeida <wdmeida@gmail.com>
2017-07-13 10:18:10 -0300

    Inclusão do i18n nas views, correção da exibição das mensagens de erros nos formulários de cadastro, login e recuperação de senha do membro.

--- escamboapp/app/assets/javascripts/backoffice.js
+++ escamboapp/app/assets/javascripts/backoffice.js
@@ -1,33 +1,35 @@
 //= require bootstrap_sb_admin_base_v2
 //= require bootbox
 
-/* Sobrescreve o data-confirm do Rails. */
-$.rails.allowAction = function (element) {
-	var message = element.attr('data-confirm');
-	if (!message) { return true; }
 
-	var opts = {
-		title: 'Confirmação',
-		message: message,
-		buttons: {
-			confirm: {
-				label: 'Sim',
-				className: 'btn-success'
-			},
-			cancel: {
-				label: 'Não',
-				className: 'btn-danger'
-			}
-		},
-		callback: function(result) {
-			if (result) {
-				element.removeAttr('data-confirm');
-				element.trigger('click.rails')
-			}
-		}
-	};
+/* Sobrescreve  data-confirm do Rails */
 
-	bootbox.confirm(opts);
+$.rails.allowAction = function(element) {
+  var message = element.attr('data-confirm');
+  if (!message) { return true; }
 
-	return false;
+  var opts = {
+    title: "Confirmação",
+    message: message,
+    buttons: {
+        confirm: {
+            label: 'Sim',
+            className: 'btn-success'
+        },
+        cancel: {
+            label: 'Não',
+            className: 'btn-danger'
+        }
+    },
+    callback: function(result) {
+      if (result) {
+        element.removeAttr('data-confirm');
+        element.trigger('click.rails')
+      }
+    }
+  };
+
+  bootbox.confirm(opts);
+
+  return false;
 }
