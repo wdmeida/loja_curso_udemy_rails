772d1bc22c4d35b8d71ba7118151cc3a3421edd6
Wagner Almeida <wdmeida@gmail.com>
2017-08-23 09:34:00 -0300

    Adicionando formatos de resposta xml e json aos detalhes do anúncio.

--- escamboapp/app/controllers/site/ad_details_controller.rb
+++ escamboapp/app/controllers/site/ad_details_controller.rb
@@ -2,5 +2,11 @@ class Site::AdDetailsController < SiteController
 	def show
 		@categories = Category.order_by_description
 		@ad = Ad.find(params[:id])
+
+		# respond_to do |format|
+		# 	format.html # Views html.erb
+		# 	format.json { render json: @ad, except: [:description, :description_md] }
+		# 	format.xml { render xml: @ad }
+		# end
 	end
 end
